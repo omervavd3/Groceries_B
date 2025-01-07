d70ced616cbfa2d1696414adc1f5fcadd2234d1e
omervavd3 <omervavd3@gmail.com>
2025-01-07 18:32:25 +0200

    product and category test

---
+++ src/test/category.test.ts
@@ -0,0 +1,95 @@
+import request from 'supertest';
+import initApp from '../server';
+import mongoose from 'mongoose';
+import ProductModel, {IProduct} from '../models/productModel';
+import CategoryModel, {ICategory} from '../models/categoryModel';
+import express, { Express } from 'express';
+
+var app: Express;
+
+beforeAll(async () => {
+    app = await initApp();
+    await ProductModel.deleteMany()
+    await CategoryModel.deleteMany()
+})
+
+afterAll(async () => {
+    await ProductModel.deleteMany()
+    await CategoryModel.deleteMany()
+    await mongoose.connection.close()
+})
+
+let _id: string = ""
+
+describe('Category tests', () => {
+    test('Get categories', async () => {
+        const res = await request(app).get('/categories')
+        expect(res.status).toBe(200)
+        expect(res.body.length).toBe(0)
+    })
+
+    test('Create category', async () => {
+        const res = await request(app).post('/categories').send({
+            name: "Fruits"
+        })
+        expect(res.status).toBe(201)
+        expect(res.body.name).toBe("Fruits")
+        _id = res.body._id
+
+        const res2 = await request(app).get('/categories')
+        expect(res2.status).toBe(200)
+        expect(res2.body.length).toBe(1)
+    })
+
+    test('Create category with existing name', async () => {
+        const res = await request(app).post('/categories').send({
+            name: "Fruits"
+        })
+        expect(res.status).not.toBe(201)
+    })
+
+    test('Get category by id', async () => {
+        const res = await request(app).get(`/categories/${_id}`)
+        expect(res.status).toBe(200)
+        expect(res.body.name).toBe("Fruits")
+    })
+
+    test('Get category by invalid id', async () => {
+        const res = await request(app).get(`/categories/123`)
+        expect(res.status).not.toBe(200)
+    })
+
+    test('Update category', async () => {
+        const res = await request(app).put(`/categories/${_id}`).send({
+            name: "Vegetables"
+        })
+        expect(res.status).toBe(200)
+        expect(res.body.name).toBe("Vegetables")
+    })
+
+    test('Update category with false id', async () => {
+        const res = await request(app).put(`/categories/123`).send({
+            name: "Vegetables"
+        })
+        expect(res.status).not.toBe(200)
+    })
+
+    test('Get category by filter', async () => {
+        const res = await request(app).get(`/categories?filter=Vegetables`)
+        expect(res.status).toBe(200)
+        expect(res.body.length).toBe(1)
+
+        const res2 = await request(app).get(`/categories?filter=Fruits`)
+        expect(res2.status).toBe(200)
+        expect(res2.body.length).toBe(0)
+    })
+
+    test('Delete category', async () => {
+        const res = await request(app).delete(`/categories/${_id}`)
+        expect(res.status).toBe(200)
+
+        const res2 = await request(app).get(`/categories`)
+        expect(res2.status).toBe(200)
+        expect(res2.body.length).toBe(0)
+    })
+})
--- src/test/product.test.ts
+++ src/test/product.test.ts
@@ -1,4 +1,3 @@
-//test for products
 import request from 'supertest';
 import initApp from '../server';
 import mongoose from 'mongoose';
@@ -28,7 +27,7 @@ const productTest: IProduct = {
     description: "This is an apple",
     price: 5,
     imageUrl: "https://www.google.com",
-    category: categoryTest._id
+    category: categoryTest
 }
 
 afterAll(async () => {
@@ -36,6 +35,8 @@ afterAll(async () => {
     await CategoryModel.deleteMany()
     await mongoose.connection.close()
 })
+
+let _id: string = ""
 
 describe('Product tests', () => {
     test('Get products', async () => {
@@ -52,7 +53,93 @@ describe('Product tests', () => {
             imageUrl: productTest.imageUrl,
             category: productTest.category
         })
+        _id = res.body._id
         expect(res.status).toBe(201)
         expect(res.body.name).toBe(productTest.name)
+
+        const res2 = await request(app).get('/products')
+        expect(res2.status).toBe(200)
+        expect(res2.body.length).toBe(1)
+    })
+
+    test('Create same product, fail', async () => {
+        const res = await request(app).post('/products').send({
+            name: productTest.name,
+            description: productTest.description,
+            price: productTest.price,
+            imageUrl: productTest.imageUrl,
+            category: productTest.category
+        })
+        expect(res.status).not.toBe(201)
+    })
+
+    test('Get product by id', async () => {
+        const res = await request(app).get(`/products/${_id}`)
+        expect(res.status).toBe(200)
+        expect(res.body.name).toBe(productTest.name)
+    })
+
+    test('Get product by id, fail', async () => {
+        const res = await request(app).get(`/products/${_id} + 1`)
+        expect(res.status).not.toBe(200)
+    })
+
+    test('Get products by category', async () => {
+        const res = await request(app).get(`/products?filter=${categoryTest._id}`)
+        expect(res.status).toBe(200)
+        expect(res.body.length).toBe(1)
+    })
+
+    test('Update product', async () => {
+        const res = await request(app).put(`/products/${_id}`).send({
+            name: "Banana",
+            description: "This is a banana",
+            price: 3,
+            imageUrl: "https://www.google.com",
+            category: categoryTest
+        })
+        expect(res.status).toBe(200)
+        expect(res.body.name).toBe("Banana")
+    })
+
+    test('Update product, fail', async () => {
+        const res = await request(app).put(`/products/${_id} + 1`).send({
+            name: "Banana",
+            description: "This is a banana",
+            price: 3,
+            imageUrl: "https://www.google.com",
+            category: categoryTest
+        })
+        expect(res.status).not.toBe(200)
+    })
+
+    test('Delete product', async () => {
+        const res = await request(app).delete(`/products/${_id}`)
+        expect(res.status).toBe(200)
+
+        const res2 = await request(app).get('/products')
+        expect(res2.status).toBe(200)
+        expect(res2.body.length).toBe(0)
+    })
+
+    test('Update product fail', async () => {
+        const res = await request(app).put(`/products/${_id}`).send({
+            name: "Banana",
+            description: "This is a banana",
+            price: 3,
+            imageUrl: "https://www.google.com",
+            category: categoryTest
+        })
+        expect(res.status).not.toBe(200)
+    })
+
+    test('Delete product, fail', async () => {
+        const res = await request(app).delete(`/products/${_id}`)
+        expect(res.status).not.toBe(200)
+    })
+
+    test('Delete product, fail 500', async () => {
+        const res = await request(app).delete(`/products/${_id} + 1`)
+        expect(res.status).not.toBe(200)
     })
 })
